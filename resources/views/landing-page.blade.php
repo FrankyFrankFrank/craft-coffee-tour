a00952c68b143c0a5d36d1812a1f6894b2ced581
Adam Frank <a.frank@vehikl.com>
2017-07-11 22:25:20 -0400

    Landing Page Additions

--- resources/views/landing-page.blade.php
+++ resources/views/landing-page.blade.php
@@ -1,33 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
+<div class="container-fluid yellow-bg">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <h1>Craft Coffee Tour</h1>
+        <div class="col-sm-5">
+            <h1>14 Incredible Coffee Stops.<br>1 Tour.</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>
+            <a class="btn btn-primary btn-lg">Join</a>
+            <p>
         </div>
     </div>
 </div>
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-6 yellow-bg">
-            <h1>Section</h1>
+        <div class="col-md-12 text-center">
+            <h2>How It Works</h2>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6 text-center">
+            <img src="https://unsplash.it/360/220"/>
+        </div>
+        <div class="col-sm-6">
+            <h3>Get Your Tour Guide Booklet</h3>
+            <p>This keepsake quality 40-page booklet is your personal guide to all the stops in the tour and your pass to enjoy some of the finest coffee drinks Toronto has to offer.</p>
+            <p>Includes lots of great information about the cafes, roasters, baristas, and coffees as well a guide to tasting and picking great coffees.</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-6 col-sm-offset-3 text-center">
+            <h3>Have new coffee experiences</h3>
+            <p>14 highly recommended locations where you can try hand-picked selections from the experts themselves.</p>
+            <p>Enjoy everything from an award-winning flat white to a VIP coffee cupping.</p>
+            <p>All together these stops make up an incredible value.</p>
+            <img src="https://unsplash.it/760/360" class="img-responsive"/>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-6 text-center">
+            <h3>Keep the Tour Going Online</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
             tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
             quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            consequat.</p>
         </div>
-        <div class="col-md-6">
-            <h1>Section</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
+        <div class="col-md-6"></div>
     </div>
 </div>
 @endsection
