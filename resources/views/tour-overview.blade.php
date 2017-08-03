3784dac3410ad8898ff8e890da165a255e34365b
Adam Frank <a.frank@vehikl.com>
2017-08-03 00:25:49 -0400

    Use new overview model

--- resources/views/tour-overview.blade.php
+++ resources/views/tour-overview.blade.php
@@ -2,5 +2,18 @@
 
 @section('content')
 
-
+<div class="container">
+    <h1 class="mt-lrg">Tour Overview</h1>
+    <div class="flex">
+    @foreach($stops as $stop)
+        <div class="tour-overview__card {{ $stop['id'] }}">
+            <h3>{{ $stop['name']  }}</h3>
+            <p>{{ $stop['description'] }}</p>
+            <div>{{ $stop['address'] }}</div>
+            <p>{{ $stop['phone'] }}</p>
+        </div>
+    @endforeach
+    </div>
+    <hr/>
+</div>
 @endsection
