dfd457098ed68b075f331fc9ccaed178cc86a1e2
Adam Frank <a.frank@vehikl.com>
2017-08-03 23:10:00 -0400

    Add all the images to the overview page

--- resources/views/tour-overview.blade.php
+++ resources/views/tour-overview.blade.php
@@ -5,12 +5,18 @@
 @section('content')
 
 <div class="container">
-    <h1 class="mt-lrg">Tour Overview</h1>
+    
+    <div class="p-relative">
+        <h1 class="mt-lrg inline-x">Tour Overview</h1>
+    </div>
     <div class="flex">
     @foreach($stops as $stop)
         <div class="tour-overview__card {{ $stop['id'] }}">
             <h3>{{ $stop['name']  }}</h3>
             <p>{{ $stop['description'] }}</p>
+            @if(isset($stop['image']))
+            <img class="img-responsive" src="{{ asset('img/' . $stop['image']) }}" alt="{{ $stop['name'] }}"/>
+            @endif
             <div class="text-bold">{{ $stop['address'] }}</div>
             <p>{{ $stop['phone'] }}</p>
         </div>
