e63e912e2f2e7da0aad11f30bc81e80b5a6e70ef
Adam Frank <a.frank@vehikl.com>
2017-08-03 23:38:53 -0400

    Reworking overview tiles

--- resources/views/tour-overview.blade.php
+++ resources/views/tour-overview.blade.php
@@ -4,24 +4,29 @@
 
 @section('content')
 
-<div class="container">
-    
-    <div class="p-relative">
-        <h1 class="mt-lrg inline-x">Tour Overview</h1>
-    </div>
-    <div class="flex">
-    @foreach($stops as $stop)
-        <div class="tour-overview__card {{ $stop['id'] }}">
-            <h3>{{ $stop['name']  }}</h3>
-            <p>{{ $stop['description'] }}</p>
-            @if(isset($stop['image']))
-            <img class="img-responsive" src="{{ asset('img/' . $stop['image']) }}" alt="{{ $stop['name'] }}"/>
-            @endif
-            <div class="text-bold">{{ $stop['address'] }}</div>
-            <p>{{ $stop['phone'] }}</p>
+<div class="container-fluid light-grey-bg">
+    <div class="container">
+        <div class="p-relative">
+            <h1 class="mt-lrg inline-x">Tour Overview</h1>
         </div>
-    @endforeach
+        <div class="mt-sm">
+            @foreach($stops as $stop)
+                <p class="stop-list mr-med"><a class="nodeco" href="{{ '#' . $stop['id']  }}">{{ $stop['name'] }}</a></p>
+            @endforeach
+        </div>
+        <div class="flex">
+        @foreach($stops as $stop)
+            <div id="{{ $stop['id'] }}" class="tour-overview__card flexcolumn white-bg mb-sm padded-tiny">
+                <h3>{{ $stop['name'] }}</h3>
+                <div class="text-bold">{{ $stop['address'] }}</div>
+                <p>{{ $stop['phone'] }}</p>
+                <p>{{ $stop['description'] }}</p>
+                @if(isset($stop['image']))
+                <img class="img-responsive" src="{{ asset('img/' . $stop['image']) }}" alt="{{ $stop['name'] }}"/>
+                @endif
+            </div>
+        @endforeach
+        </div>
     </div>
-    <hr/>
 </div>
 @endsection
