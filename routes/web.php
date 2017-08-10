493382b69a078782e929df8f3fe2c95316425679
Adam Frank <a.frank@vehikl.com>
2017-08-09 22:01:30 -0400

    Short name redirect

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,18 @@ Auth::routes();
 Route::get('/', function(){ return view('landing-page'); })->name('landing');
 
 Route::get('/overview', function(){ 
-	$stops = app('App\TourOverview')->stops;
-	return view('tour-overview', ['stops' => $stops]);
+	$locations = App\Location::all();
+	return view('tour-overview', ['locations' => $locations]);
 })->name('overview');
 
 Route::get('/locations', 'LocationController@index');
 Route::get('/locations/{id}', 'LocationController@show');
-
 Route::get('/rsvp', 'RsvpController@index');
-
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/{shortname}', function($shortname){
+	$location = App\Location::where('shortname', $shortname)->first();
+    if (!$location) {
+        return redirect()->route('landing');
+    }
+    return redirect('/locations/' . $location->id);
+});
