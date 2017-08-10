176f23f02ffc6ed96fedf21129d9ab6b06cdab14
Adam Frank <a.frank@vehikl.com>
2017-08-10 19:26:56 -0400

    Rename migration

--- database/migrations/2017_08_09_033602_create_locations_table.php
+++ database/migrations/2017_08_09_033602_create_locations_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLocationsTableSecond extends Migration
+class CreateLocationsTable extends Migration
 {
     /**
      * Run the migrations.
